be5b102019a7104a99d6147858e76068ea1bf380
krsagn <agenakristian@gmail.com>
2026-02-26 01:40:12 +0800

    refactor: simplify context menu component and update styling

--- client/src/shared/ui/index.ts
+++ client/src/shared/ui/index.ts
@@ -13,3 +13,4 @@ export { Separator } from "./separator";
 export { Spinner } from "./spinner";
 export { Modal, ModalBackdrop } from "./modal";
 export * from "./alert-dialog";
+export * from "./context-menu";
